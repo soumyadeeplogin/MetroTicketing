5ff7c75c9aaaa79b84cbd57d4cb87a87c22d87a4
Soumyadeep <Soumyadeep@Roy-HP>
2017-03-31 23:59:13 +0530

    ...

--- src/com/metro/test/Runner.java
+++ src/com/metro/test/Runner.java
@@ -5,8 +5,8 @@ import com.metro.main.Flow;
 public class Runner {
 
 	public static void main(String[] args) {
-		//new Flow();
-		new Flow("A4", "B12");
+		new Flow();
+		/*new Flow("A4", "B12");
 		new Flow("A1", "A2");
 		new Flow("A1", "B1");
 		new Flow("A1", "C1");
@@ -14,7 +14,7 @@ public class Runner {
 		new Flow("A1", "X1");
 		new Flow("B1", "X1");
 		new Flow("C1", "X1");
-		new Flow("X1", "X2");
+		new Flow("X1", "X2");*/
 	}
 
 }
